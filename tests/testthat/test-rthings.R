f68179bed76d76c628c360691457de1d3353d9ff
Karthik Ram <karthik.ram@gmail.com>
2017-04-28 08:56:00 -0700

    Very basic tests

--- tests/testthat/test-rthings.R
+++ tests/testthat/test-rthings.R
@@ -1,6 +1,5 @@
-library(testthat)
 context("We can get a list")
 test_that("we can get a character list back", {
-   # x <- rthings::stuff()
-   # expect_is(x, "character")
+    x <- rthings::stuff()
+    expect_is(x, "character")
 })
